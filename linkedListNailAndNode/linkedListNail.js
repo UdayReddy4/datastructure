3b91f9e509098cdeb044808244432b88481b466b
UdayReddy4 <udayreddykr8339@gmail.com>
2024-04-08 18:29:58 +0530

    linkedListNode

---
+++ linkedListNailAndNode/linkedListNail.js
@@ -0,0 +1,37 @@
+class SinglyLinkedListNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+function insertNodeAtTail(head, data) {
+    if (!(head instanceof SinglyLinkedListNode)) {
+        throw new Error('Invalid head parameter: head must be a SinglyLinkedListNode instance');
+    }
+
+    if (typeof data !== 'number' || isNaN(data)) {
+        throw new Error('Invalid data parameter: data must be a valid number');
+    }
+
+    const newNode = new SinglyLinkedListNode(data);
+
+}
+    
+    if (head === null) {
+        head = newNode;
+    } else {
+        let current = head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    return head;
+
+
+    
+
+
+module.exports=insertNodeAtTail
